3a3135f7986859de91a714a57d4eaabec500b2f2
Jean-Pierre MENA <pythonrubylang@gmail.com>
2013-11-18 07:27:54 +0100

    Passage à la moyenne flottante pour calculer la vitesse

--- domaine/GPXHandler.py
+++ domaine/GPXHandler.py
@@ -46,7 +46,10 @@ def recupere_donnees(element):
             try:
                 retour['date']=datetime.strptime(ele.text,'%Y-%m-%dT%H:%M:%SZ')
             except ValueError as ve:
-                print("impossible de recupere la date du point: message {0}\n".format(ve))
+                try:
+                    retour['date']=datetime.strptime(ele.text,'%Y-%m-%dT%H:%M:%S.1Z')
+                except ValueError as ve:
+                    print("impossible de recupere la date du point: message {0}\n".format(ve))
     return retour
 
 def calcule_distance_parcourue(donnees_avant,donnees_actuelles):
@@ -67,17 +70,20 @@ def calcule_distance_parcourue(donnees_avant,donnees_actuelles):
                     t2 = math.cos(lat1) * math.cos(lat2);
                     t4 = t2 * t3;
                     t5 = t1 + t4;
-                    rad_dist = math.atan(-t5 / math.sqrt(-t5 * t5 + 1)) + 2 * math.atan(1);
-                    #we get a distance in meters
-                    distance = (rad_dist * 3437.74677 * 1.1508) * 1.6093470878864446 * 1000
-                    if 'date' in donnees_avant.keys() and donnees_avant['date'] is not None:
-                        if 'date' in donnees_actuelles.keys() and donnees_actuelles['date'] is not None:
-                            delta = donnees_actuelles['date'] - donnees_avant['date']
-                            vitesse = distance / delta.total_seconds() #speed in meter/second
+                    if -t5 * t5 + 1 != 0:
+                        rad_dist = math.atan(-t5 / math.sqrt(-t5 * t5 + 1)) + 2 * math.atan(1);
+                        #we get a distance in meters
+                        distance = (rad_dist * 3437.74677 * 1.1508) * 1.6093470878864446 * 1000
+                        if 'date' in donnees_avant.keys() and donnees_avant['date'] is not None:
+                            if 'date' in donnees_actuelles.keys() and donnees_actuelles['date'] is not None:
+                                delta = donnees_actuelles['date'] - donnees_avant['date']
+                                vitesse = distance / delta.total_seconds() #speed in meter/second
+                            else:
+                                print ("timestamp du point 2 absente\n")
                         else:
-                            print ("timestamp du point 2 absente\n")
+                            print ("timestamp du point 1 absente\n")
                     else:
-                        print ("timestamp du point 1 absente\n")
+                        print ("on ne peut calculer la distance car diviseur =0\n")
                 else:
                     print ("latitude du point 2 absente\n")
             else:
@@ -88,53 +94,64 @@ def calcule_distance_parcourue(donnees_avant,donnees_actuelles):
         print ("longitude du point 1 absente\n")
     return (distance , vitesse)
 if __name__ == '__main__':
-    path_gpx = "/home/jpmena/RSM/GPXTEsts/GPXFiles/seance du 21 oct 2013.gpx"
+    path_gpx = "/home/jpmena/RSM/GPXTEsts/GPXFiles/Solene/aulnay 16 nov 2013.gpx"
     points=[]
     for domElem in parse_and_remove(path_gpx):
         points.append(recupere_donnees(domElem))
     print ("il y a {0} points\n".format(len(points)))
     #nous allons calculer la distance cumulée et l'enregistrer(la formule nous a retourné la distance directement en kms)
-    dout_path='%s.dplot' %(path_gpx)
-    dplot = open(dout_path, 'w')
-    vout_path='%s.vplot' %(path_gpx)
-    vplot = open(vout_path, 'w')
+    iout_path='%s.inst_splot' %(path_gpx)
+    iplot = open(iout_path, 'w')
+    lout_path='%s.lissee_plot' %(path_gpx)
+    lplot = open(lout_path, 'w')
     avancees = []
-    vitesses = []
-    pavant = None
     prise_vitesse_avant = (None,0)  #le datetime de prise de la vitesse avant et la distance cumulée correspondante
-    davant=0
-    calcul_vitesse_tous_les_m = 50 #on ne meseure la vitesse que tous les 50m
+    nombre_points_lissage = 10 #on ne meseure la vitesse que tous les 50m
+    zone_lissage_points = []
+    pAvant = None
     for p in points:
-        avancee = {}
-        velem = {}
-        delta = 0
-        vitesse = -1
-        if pavant is not None:
-            (delta,vitesse) = calcule_distance_parcourue(pavant,p)
+        if pAvant is None:
+            pAvant = p
         else:
-            prise_vitesse_avant = (p['date'],0)
-        avancee['d'] = delta
-        avancee['v'] = vitesse
-        avancee['dc'] = davant + delta
-        avancee['t'] = p['date']
-        delta_pour_vitesse = avancee['dc'] - prise_vitesse_avant[1]
-        if delta_pour_vitesse >= (calcul_vitesse_tous_les_m) and avancee['v'] > 0 and prise_vitesse_avant[0] is not None: 
-            velem['t'] = p['date']
-            delta_t = velem['t'] - prise_vitesse_avant[0]
-            velem['dc'] = avancee['dc']
-            velem['v'] = delta_pour_vitesse / (delta_t.total_seconds())
-            vitesses.append(velem)
-            vplot.write("{0} {1} {2}\n".format(velem['t'].strftime("%H:%M:%S"),velem['dc']/1000,velem['v']*3.6))
-            print ("{0} {1} {2}\n".format(velem['t'].strftime("%H:%M:%S"),velem['dc']/1000,velem['v']*3.6))
-        pavant = p
-        davant = avancee['dc']
-        avancees.append(avancee)
-        dplot.write("{0} {1} {2}\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['v']*3.6))
-        print("{0} {1} {2}\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['v']*3.6))        
+            avancee = {}
+            pEnCours = calcule_distance_parcourue(pAvant,p)
+            if pEnCours[0] is not None and pEnCours[1] > 0:
+                avancee['t'] = p['date']
+                avancee['d'] = pEnCours[0]
+                avancee['v'] = pEnCours[1]
+            #on va tester le SMA: http://en.wikipedia.org/wiki/Moving_average
+            nbPointsEnCours = len(zone_lissage_points)
+            N = nbPointsEnCours + 1 
+            if len(avancee) > 0 and nbPointsEnCours > nombre_points_lissage:
+                pointAncien = zone_lissage_points[0]
+                avancee['vl'] = (- pointAncien['vl'] + pEnCours[1]) / N + zone_lissage_points[-1]['vl']
+                avancee['dc'] = zone_lissage_points[-1]['dc'] + avancee['d']
+                zone_lissage_points.append(avancee)
+                zone_lissage_points = zone_lissage_points[1:]
+            elif len(avancee) > 0 and nbPointsEnCours > 0:
+                vitesse_moyenne = 0
+                for ppt in zone_lissage_points:
+                    vitesse_moyenne += ppt['vl']
+                vitesse_moyenne += avancee['v']
+                avancee['vl'] = vitesse_moyenne / N
+                avancee['dc'] = zone_lissage_points[-1]['dc'] + avancee['d']
+                zone_lissage_points.append(avancee)
+            elif len(avancee) > 0:
+                avancee['vl'] = pEnCours[1]
+                avancee['dc'] = avancee['d']
+                zone_lissage_points.append(avancee)
+            if len(avancee) > 0:
+                avancees.append(avancee)
+                pAvant = p
+                iplot.write("{0} {1} {2} vitesse_instant\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['v']*3.6))
+                print ("{0} {1} {2} vitesse_instant\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['v']*3.6))
+                lplot.write("{0} {1} {2} vitesse_lissee\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['vl']*3.6))
+                print("{0} {1} {2} vitesse_lissee\n".format(avancee['t'].strftime("%H:%M:%S"),avancee['dc']/1000,avancee['vl']*3.6))
+            else:
+               print("distance parouru None, point négligeable ...") 
     print ("il y a {0} distances calculées\n".format(len(avancees)))
-    print ("il y a {0} vitesses calculées\n".format(len(vitesses)))
-    dplot.close()
-    vplot.close()
+    iplot.close()
+    lplot.close()
         
     
         #print 'longitude {lon}'.format({'lon':repr(domElem)})
